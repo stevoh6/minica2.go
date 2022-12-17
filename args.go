a279580270f1ae51cdc2529401e10811676ca3f5
Fred Brennan <copypaste@kittens.ph>
2022-12-16 23:41:49 -0500

    Split main.go into main.go / ca.go

--- args.go
+++ args.go
@@ -4,6 +4,7 @@ import (
 	"crypto/x509/pkix"
 	"flag"
 	"fmt"
+	"os"
 	"net"
 	"regexp"
 	"strings"
@@ -82,5 +83,43 @@ func (args *Args) parseIssuer() (pkix.Name) {
 		Locality: args.ilocale.a,
 		StreetAddress: args.iaddress.a,
 		PostalCode: args.ipostal.a,
+	}
+}
+
+func (args *Args) parse() {
+	args.assignFlags()
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, `
+Minica is a simple CA intended for use in situations where the CA operator
+also operates each host where a certificate will be used. It automatically
+generates both a key and a certificate when asked to produce a certificate.
+It does not offer OCSP or CRL services. Minica is appropriate, for instance,
+for generating certificates for RPC systems or microservices.
+
+On first run, minica will generate a keypair and a root certificate in the
+current directory, and will reuse that same keypair and root certificate
+unless they are deleted.
+
+On each run, minica will generate a new keypair and sign an end-entity (leaf)
+certificate for that keypair. The certificate will contain a list of DNS names
+and/or IP addresses from the command line flags. The key and certificate are
+placed in a new directory whose name is chosen as the first domain name from
+the certificate, or the first IP address if no domain names are present. It
+will not overwrite existing keys or certificates.
+
+`)
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+	var domains = args.domains.a
+	var ipAddresses = args.ipAddresses.a
+	if len(domains) == 0 && len(ipAddresses) == 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
+	if len(flag.Args()) > 0 {
+		fmt.Printf("Extra arguments: %s (maybe there are spaces in your domain list?)\n", flag.Args())
+		os.Exit(1)
 	}
 }
